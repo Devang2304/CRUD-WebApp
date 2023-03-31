80759fec695dba6720cdffcd5abfc58dbdfb8bac
Devang2304 <devangvartak2304@gmail.com>
2023-04-01 01:21:27 +0530

    created table input for get request

--- client/src/components/AllUsers.jsx
+++ client/src/components/AllUsers.jsx
@@ -1,8 +1,42 @@
+import {Table,TableHead,TableBody,TableRow,TableCell} from '@mui/material';
+import {getUsers} from '../api/api';
+import { useEffect,useState } from 'react';
 
 
 const AllUsers=()=>{
+    const [users,setUsers]=useState([]);
+
+    useEffect(()=>{
+        getAllusers();
+    },[]);
+    const getAllusers=async()=>{
+       let response =await getUsers();
+       setUsers(response.data);
+    }
     return(
-        <p>This is AllUsers</p>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Id</TableCell>
+                    <TableCell>Name</TableCell>
+                    <TableCell>Phone Number</TableCell>
+                    <TableCell>Email</TableCell>
+                    <TableCell>Hobbies</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {
+                    users.map((user)=>(
+                        <TableRow>
+                            <TableCell>{user.name}</TableCell>
+                            <TableCell>{user.phoneNumber}</TableCell>
+                            <TableCell>{user.email}</TableCell>
+                            <TableCell>{user.hobbies}</TableCell>
+                        </TableRow>
+                    ))
+                }
+            </TableBody>
+        </Table>
     );
 }
 
