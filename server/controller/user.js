e11e5c995502f4e31ffce625deb1efa70ae1b3f2
Devang2304 <devangvartak2304@gmail.com>
2023-04-01 01:24:10 +0530

    added get and post request for DB

--- server/controller/user.js
+++ server/controller/user.js
@@ -1,6 +1,6 @@
-const AddUserDetails = require('../model/model.js');
+import AddUserDetails from '../model/model.js';
 
-const addUser = async (req, res) => {
+export const addUser = async (req, res) => {
     const user =req.body;
 
     const newUser = new AddUserDetails(user);
@@ -13,4 +13,12 @@ const addUser = async (req, res) => {
     }
 }
 
-module.exports = addUser;
+export const getUsers =async(req,res)=>{
+    try {
+        const users= await AddUserDetails.find({});
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
