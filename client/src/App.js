7b4d471d29d131618c72e24b0808e87b264e90dc
Devang2304 <devangvartak2304@gmail.com>
2023-04-07 22:34:27 +0530

    added route for editUser

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import NavBar from "./components/NavBar";
 import CrudApp from "./components/CrudApp";
 import AddUser from "./components/AddUser";
 import AllUsers from "./components/AllUsers";
+import EditUser from "./components/EditUser";
+
 
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 function App() {
@@ -13,6 +15,7 @@ function App() {
         <Route path="/" element={<CrudApp/>}/>
         <Route path="/all" element={<AllUsers/>}/>
         <Route path="/add"  element={<AddUser/>}/>
+        <Route path="/edit/:id"  element={<EditUser/>}/>
       </Routes>
     </BrowserRouter>
   );
