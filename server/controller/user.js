70ddae70707433a41541836d1ff207b6877798db
Devang2304 <devangvartak2304@gmail.com>
2023-04-07 22:39:19 +0530

    added getUser function

--- server/controller/user.js
+++ server/controller/user.js
@@ -22,3 +22,12 @@ export const getUsers =async(req,res)=>{
     }
 }
 
+export const getUser = async(req, res) => {
+    try {
+        const user = await AddUserDetails.findById(req.params.id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
